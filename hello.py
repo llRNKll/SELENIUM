c49c85f97cc6019b31d115fd305ae6fe42899f93
llRNKll <adnan.ronok@gmail.com>
2022-12-14 00:03:49 +0600

    updated

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-#Importing packages from SELENIUM, we will work with "Chrome Browser"
+'''#Importing packages from SELENIUM, we will work with "Chrome Browser"
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,3 +9,17 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.google.com')
+'''
+
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://getintopc.com/softwares/audio-processing/syrinxsamples-cs15d-kontakt-free-download/')
+driver.find_element(By.CLASS_NAME, 'btn').click()
